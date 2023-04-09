5358337dcf2f2ea08c8917f5f2869c367a8214eb
Kevin Channon <kevinchannon@gmail.com>
2023-04-09 08:18:38 +0100

    Applying formatting to udp/types.hpp

--- raven/udp/types.hpp
+++ raven/udp/types.hpp
@@ -13,11 +13,11 @@ constexpr auto any_port = port_number{0};
 
 template <typename T>
 concept contiguous_byte_range_like = requires(T& t) {
-                            std::ignore = wite::io::is_byte_like_v<typename std::decay_t<T>::value_type>;
-                            std::ignore = t.begin();
-                            std::ignore = t.end();
-                            std::ignore = t.size();
-                            std::ignore = t.data();
-                          };
+                                       std::ignore = wite::io::is_byte_like_v<typename std::decay_t<T>::value_type>;
+                                       std::ignore = t.begin();
+                                       std::ignore = t.end();
+                                       std::ignore = t.size();
+                                       std::ignore = t.data();
+                                     };
 
-}
+}  // namespace raven::udp
