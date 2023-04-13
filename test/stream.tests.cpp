8be456792ef2ebe4350b5ba7c32e6fa25ccafb0f
Kevin Channon <kevinchannon@gmail.com>
2023-04-13 16:51:24 +0100

    Tidy streams test

--- test/stream.tests.cpp
+++ test/stream.tests.cpp
@@ -5,13 +5,13 @@
 #include <nsl/udp/types.hpp>
 
 #include "test/io_runner.hpp"
+#include "test/waiting.hpp"
 
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/generators/catch_generators.hpp>
-
 #include <nlohmann/json.hpp>
-
 #include <boost/asio.hpp>
+#include <wite/collections/make_vector.hpp>
 
 #include <algorithm>
 #include <random>
@@ -24,9 +24,6 @@ using namespace nsl;
 TEST_CASE("reading and writing to UDP streams") {
   auto io = boost::asio::io_context{};
 
-  auto mtx                 = std::mutex{};
-  auto recv_thread_running = std::condition_variable{};
-
   constexpr auto test_port = udp::port_number{40000};
 
   SECTION("blocking streams") {
@@ -34,47 +31,36 @@ TEST_CASE("reading and writing to UDP streams") {
     auto udp_out = udp::ostream{"localhost", test_port};
 
     SECTION("writing and reading JSON") {
-      auto recv_json = json{};
       auto sent_json = json::parse(R"({"bool_field": true, "int_field": 12345, "string_field": "ahoy there!"})");
 
-      auto lock          = std::unique_lock<std::mutex>{mtx};
-      auto data_received = std::async([&]() {
-        auto lock = std::unique_lock<std::mutex>{};
-        recv_thread_running.notify_all();
-
+      auto recv_data = test::running_async([&]() {
+        auto recv_json = json{};
         udp_in >> recv_json;
+        return recv_json;
       });
-
-      recv_thread_running.wait(lock);
 
       udp_out << sent_json << std::endl;
 
-      data_received.wait();
-
-      REQUIRE(sent_json == recv_json);
+      REQUIRE(sent_json == recv_data.get());
     }
 
     SECTION("writing and reading 1024 bytes") {
-      auto sent_bytes  = std::vector<std::byte>{};
+      auto sent_bytes = wite::make_vector<std::byte>(wite::arg::reserve{1024});
+
       auto rng         = std::mt19937{345234};  // arbitrary seed.
-      auto random_byte = std::uniform_int<>{0x00, 0xFF};
-      std::generate_n(std::back_inserter(sent_bytes), 1024, [&]() { return static_cast<std::byte>(random_byte(rng)); });
-      auto recv_bytes  = std::vector<std::byte>(sent_bytes.size(), std::byte{0});
-
-      auto lock          = std::unique_lock<std::mutex>{mtx};
-      auto data_received = std::async([&]() {
-        auto lock = std::unique_lock<std::mutex>{};
-        recv_thread_running.notify_all();
-
-        udp_in >> recv_bytes;
+      std::generate_n(std::back_inserter(sent_bytes), 1024, [&]() {
+        return static_cast<std::byte>(std::uniform_int<>{0x00, 0xFF}(rng));
       });
 
-      recv_thread_running.wait(lock);
+      auto recv_bytes = test::running_async([&]() {
+        auto recv_bytes = std::vector<std::byte>(sent_bytes.size(), std::byte{0});
+        udp_in >> recv_bytes;
+        return recv_bytes;
+      });
+
       udp_out << sent_bytes << udp::flush;
 
-      data_received.wait();
-
-      REQUIRE(std::ranges::equal(sent_bytes,recv_bytes));
+      REQUIRE(std::ranges::equal(sent_bytes, recv_bytes.get()));
     }
   }
 }
